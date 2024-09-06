2364bb8421460645f6801d00d876c6a21984de36
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2024-09-06 05:53:42 +0300

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,51 @@
 #!/usr/bin/node
 /**
- * Starwars Characters
+ * Star Wars Characters
  */
 const request = require('request');
 
+// Get the movie ID from the command-line arguments
 const movieId = process.argv[2];
+if (!movieId) {
+  console.log('Usage: ./star_wars_characters.js <movieId>');
+  process.exit(1);
+}
+
 const movieURL = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(movieURL, (err, res, body) => {
-  if (err) console.log(err);
-  const index = 0;
-  const characters = JSON.parse(body).characters;
-  printMovieCharacter(characters, index);
-});
+// Function to print the characters recursively
+const printMovieCharacter = function (urls, i) {
+  if (i >= urls.length) {
+    return; // Base case: stop if all characters are printed
+  }
 
-const printMovieCharacter = function (url, i) {
-  request(url[i], (err, res, body) => {
-    if (err) console.log(err);
-    console.log(JSON.parse(body).name);
-    if (++i < url.length) {
-      printMovieCharacter(url, i++);
+  request(urls[i], (err, res, body) => {
+    if (err) {
+      console.log(err);
+      return;
     }
+    try {
+      const character = JSON.parse(body);
+      console.log(character.name);
+    } catch (err) {
+      console.log('Error parsing character data:', err);
+    }
+
+    // Recursive call to print the next character
+    printMovieCharacter(urls, i + 1);
   });
 };
+
+// Make a request to get the movie details
+request(movieURL, (err, res, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  try {
+    const characters = JSON.parse(body).characters;
+    printMovieCharacter(characters, 0); // Start printing characters from index 0
+  } catch (err) {
+    console.log('Error parsing movie data:', err);
+  }
+});
